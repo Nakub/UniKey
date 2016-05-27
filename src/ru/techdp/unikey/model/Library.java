bfe48cae77aa42c143ed38d8210ffadfcc760cfc
Aleksandr Fridenberg <prog@techdp.ru>
2016-05-27 20:37:46 +0400

    Library now uses ArrayList. Delete commented old implementation of getCommandID

--- src/ru/techdp/unikey/model/Library.java
+++ src/ru/techdp/unikey/model/Library.java
@@ -1,35 +1,24 @@
 package ru.techdp.unikey.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
-/**
- * Created by admin on 22.05.2016.
- */
+
 public class Library {
 
-    private final String[] commands;
+    private final ArrayList commands;
 
 
-    public Library(String[] commands) {
-        Arrays.sort(commands);
+    public Library(ArrayList commands) {
         this.commands = commands;
     }
 
     public String getCommand(int commandID) {
-        return commands [commandID];
+        return (String)commands.get(commandID);
     }
 
-//    public int getCommandID(String command) {
-//        String value;
-//        int i = 0;
-//        do {value = commands[i];
-//            i++;
-//        } while (!command.equals(value));
-//        return i-1;
-//
-//    }
     public int getCommandID(String command) {
-        return Arrays.binarySearch(this.commands, command);
+        return commands.indexOf(command) ;
     }
 
 }
