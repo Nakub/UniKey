e1ac920c133077223130c89ecc426445166be9c9
Aleksandr Fridenberg <prog@techdp.ru>
2016-05-27 20:32:36 +0400

    Glossary now uses ArrayList

--- src/ru/techdp/unikey/model/Glossary.java
+++ src/ru/techdp/unikey/model/Glossary.java
@@ -1,20 +1,19 @@
 package ru.techdp.unikey.model;
 
 
-/**
- * Created by admin on 22.05.2016.
- */
+import java.util.ArrayList;
+
 public class Glossary {
 
-    private final int[] outCommandID;
+    private final ArrayList outCommandID;
 
 
-    public Glossary(int[] outCommandID) {
+    public Glossary(ArrayList outCommandID) {
         this.outCommandID = outCommandID;
     }
 
     public int getOutCommandID(int inCommandID) {
-       return outCommandID [inCommandID];
+       return (int)outCommandID.get(inCommandID);
     }
 
 }
